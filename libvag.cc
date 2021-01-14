7c9a9dade17f94cfd5549e78e70350d288642661
paulyc <paulyc@quetzalcoatl.us>
2021-01-14 10:58:33 -0500

    get some values in there

--- libvag.cc
+++ libvag.cc
@@ -1,1 +1,39 @@
 #include "libvag.h"
+
+#include <cstring>
+
+void set_vag_data_size(struct VAGHeader *hdr, uint32_t data_size_bytes) {
+    hdr->DataSizeBytes[0] = data_size_bytes & (0xFF << 24);
+    hdr->DataSizeBytes[1] = data_size_bytes & (0xFF << 16);
+    hdr->DataSizeBytes[2] = data_size_bytes & (0xFF << 8);
+    hdr->DataSizeBytes[3] = data_size_bytes & (0xFF << 0);
+}
+
+void set_vag_sample_frequency(struct VAGHeader *hdr, uint32_t sample_frequency_hz) {
+    hdr->SampleFreqHz[0] = sample_frequency_hz & (0xFF << 24);
+    hdr->SampleFreqHz[1] = sample_frequency_hz & (0xFF << 16);
+    hdr->SampleFreqHz[2] = sample_frequency_hz & (0xFF << 8);
+    hdr->SampleFreqHz[3] = sample_frequency_hz & (0xFF << 0);
+}
+
+char* set_vag_name(struct VAGHeader *hdr, char *name) {
+    return strncpy(hdr->Name, name, sizeof(hdr->Name));
+}
+
+size_t prepend_header_and_dump(char *vb_filename) {
+    size_t written = 0;
+    char buf[4096];
+    struct VAGHeader hdr;
+    FILE *f = fopen(vb_filename, "rb");
+    fseek(f, 0, SEEK_END);
+    uint32_t data_size_bytes = ftell(f);
+    set_vag_data_size(&hdr, data_size_bytes);
+    fseek(f, 0, SEEK_SET);
+    written += fwrite(&hdr, 1, sizeof(VAGHeader), stdout);
+    while (!feof(f)) {
+        size_t rd = fread(buf, sizeof(buf), sizeof(char), f);
+        written += fwrite(buf, sizeof(buf), sizeof(char), stdout);
+    }
+    fclose(f);
+    return written;
+}
--- libvag.h
+++ libvag.h
@@ -10,21 +10,25 @@
 extern "C" {
 #endif
 
-// BIIIG ENDIAN, LIKE MIPS!!!
+// BIIIIG ENDIAN, LIKE MIPS!!!
+// SO IM ONLY USING CHARS SO YOU HAVE TO TRY EXTRA HARD TO F**K IT UP
 struct VAGHeader {
-    uint32_t Magick;        // 'VAGp' or 'pGAV'
-    uint32_t Version;       // 0x00000006 = 0.6 ?
-    uint8_t  Reserved0[4];  // 0
-    uint32_t DataSizeBytes;
-    uint32_t SamplingFrequencyHz;
-    uint8_t  Reserved1[10]; // 0
-    uint8_t  NumChannels;   // 0-1 = "mono", 2 = "stereo"
-    uint8_t  Reserved2;     // 0
-    char     Name[16];      // Zero Terminated or can it be 16 actual characters? What encoding? IDK
+    char    Magick[4]        = {'V', 'A', 'G', 'p'}; // 'VAGp' or 'pGAV'
+    uint8_t Version[4]       = {0, 0, 0, 6};    // 0.6 ?
+    uint8_t Reserved0[4]     = {0};
+    uint8_t DataSizeBytes[4] = {0};             // FILL ME IN
+    uint8_t SampleFreqHz[4]  = {0, 0, 0x7d, 0}; // 32000 Hz
+    uint8_t Reserved1[10]    = {0};
+    uint8_t NumChannels      =  2;              // 0-1 = "mono", 2 = "stereo"
+    uint8_t Reserved2        =  0;
+    char    Name[16]         = "NAMENAMENAMENAM"; // Zero Terminated or can it be 16 actual characters? What encoding? IDK
 } __attribute((packed));
 static_assert(sizeof(struct VAGHeader) == 0x30);
 
-int write_vag_header(FILE *f, uint32_t data_size_bytes, uint32_t sampling_frequency_hz, uint8_t num_channels, char name[0x20]);
+void set_vag_data_size(struct VAGHeader *hdr, uint32_t data_size_bytes);
+void set_vag_sample_frequency(struct VAGHeader *hdr, uint32_t sample_frequency_hz);
+char* set_vag_name(struct VAGHeader *hdr, char *name);
+size_t prepend_header_and_dump(char *vb_filename);
 
 #ifdef __cplusplus
 }
